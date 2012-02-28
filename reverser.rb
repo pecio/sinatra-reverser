cae76f359f467e797a5688bbe25c68335325e4b7
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2012-02-28 10:44:28 +0100

    UTF-8 support test.

--- reverser.rb
+++ reverser.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'sinatra'
 get "/" do
   erb :form
@@ -5,4 +6,8 @@ end
 
 post "/reversed" do
   erb :reversed
+end
+
+get "/test" do
+  "@peña, ¿Qué #pasa?".gsub(/([@#]\w+)/u, '<b>\1</b>')
 end
